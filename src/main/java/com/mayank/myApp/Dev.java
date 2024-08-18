e778cc685be140c05dead40a7f68b11b407054f3
Mayank Srivastava <rsmayank25@outlook.com>
2024-08-18 15:22:42 +0530

    initial Commit

---
+++ src/main/java/com/mayank/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.mayank.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("Hello Aliens");
+    }
+
+}
