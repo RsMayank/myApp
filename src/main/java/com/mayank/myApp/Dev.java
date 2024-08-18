76bb7a7bf912f6383cd6370f693d37ebbbdbdbb9
Mayank Srivastava <rsmayank25@outlook.com>
2024-08-18 16:38:22 +0530

    Injection

--- src/main/java/com/mayank/myApp/Dev.java
+++ src/main/java/com/mayank/myApp/Dev.java
@@ -1,12 +1,31 @@
 package com.mayank.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
+    // Accessing laptop class   
+    @Autowired
+    private Laptop laptop;
+
     public void build(){
         System.out.println("Hello Aliens");
+        laptop.compile();
     }
+
+
+    //contructor injection
+    // public Dev(Laptop laptop){
+    //     this.laptop = laptop;
+    // }
+
+    // Setter injection
+
+    // public void setLaptop(Laptop laptop){
+    //     this.laptop = laptop;
+    // }
+
 
 }
